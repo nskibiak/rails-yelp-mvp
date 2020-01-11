4b3ad1b052979c82ea438de4d2fc080e4a83e4bc
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-11 12:21:28 +0900

    added validation to Restaurant class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
 end
